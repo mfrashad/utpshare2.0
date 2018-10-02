938a63525bf4b47a1dd79f8607e00fdd18523ca0
mfrashad <m.fathyrashad@gmail.com>
2018-10-02 19:25:37 +0800

    Update category ui to support new subcategory model

--- src/products/views.py
+++ src/products/views.py
@@ -59,7 +59,7 @@ class ProductListView(ListView):
 
   def get_context_data(self, **kwargs):
     context = super(ProductListView, self).get_context_data(**kwargs)
-    context['category_list'] = Category.objects.filter(parent_category=None)
+    context['category_list'] = Category.objects.all()
     return context 
 
 
