5f17e1051d0566d2334b4e695704a676e6c54dd9
mfrashad <m.fathyrashad@gmail.com>
2018-10-02 18:49:26 +0800

    Register subcategory to admin and add parent_category filter to Subcategory admin

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, ProductImage, Category, Tag
+from .models import Product, ProductImage, Category, Subcategory, Tag
 
 # Register your models here.
 class ProductImageInline(admin.TabularInline):
@@ -20,7 +20,12 @@ class ProductImageAdmin(admin.ModelAdmin):
   class Meta:
     model = ProductImage
 
+class SubcategoryAdmin(admin.ModelAdmin):
+  list_display = ["__str__", "parent_category"]
+  list_filter = ["parent_category"]
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductImage, ProductImageAdmin)
 admin.site.register(Category)
+admin.site.register(Subcategory, SubcategoryAdmin)
 admin.site.register(Tag)
